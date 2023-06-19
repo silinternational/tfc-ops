dc61039339caf882f49e071858c3f267c87f85b6
briskt <3172830+briskt@users.noreply.github.com>
2023-06-19 16:34:06 +0800

    missed "workspace" in the request schema

--- lib/run_test.go
+++ lib/run_test.go
@@ -6,7 +6,7 @@ import (
 
 func Test_buildRunPayload(t *testing.T) {
 	got := buildRunPayload("my message", "ws_id")
-	if got != `{"data":{"attributes":{"message":"my message"},"relationships":{"data":{"id":"ws_id"}}}}` {
+	if got != `{"data":{"attributes":{"message":"my message"},"relationships":{"workspace":{"data":{"id":"ws_id"}}}}}` {
 		t.Fatalf("did not get expected result, got %q", got)
 	}
 }
